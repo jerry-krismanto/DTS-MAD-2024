eb0e3c8a22f94b89c35e9d51af61718cb92028d3
Flabib <fahdilabib@gmail.com>
2024-05-23 19:48:51 +0700

    Done adding feature A

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 let print = console.log
 
 print('Hello, kelas D!')
+
+let siswa = [
+    "Billi",
+    "Syawal",
+    "Jerry",
+    "Fikri",
+    "Zahra",
+]
+
+print('Di kelas ini, ada siswa bernama', siswa)
